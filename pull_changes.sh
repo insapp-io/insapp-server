4e011f32b891b29bbf9574d1f763998864425ee5
Thomas Bouvier <thomas.bouvier@insa-rennes.fr>
2018-01-15 00:24:35 +0100

    Added script to pull changes

---
+++ pull_changes.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Stop running containers
+docker-compose stop
+
+# Pull changes for each submodule
+git pull && git submodule foreach git pull origin master
+
+# Build images and launch them
+docker-compose build
+docker-compose up -d
+
+echo "Changes from master have been successfully pulled."
