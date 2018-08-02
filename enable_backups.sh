926962bf36a2d4c3b4e516d220cd2dedcdb4329d
Thomas Bouvier <tomatrocho@gmail.com>
2018-08-02 22:13:40 +0200

    Fixed paths

--- enable_backups.sh
+++ enable_backups.sh
@@ -3,10 +3,10 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 
-# Create CRON jobs
+# Creating CRON jobs
 
 crontab -l | { cat; echo "30 3 * * * docker exec insapp-server_db_1 mongodump -u insapp-admin -p REPLACE_WITH_THE_MONGO_PASSWORD --authenticationDatabase admin --out /data/db/backups/`date +%Y-%m-%d` --db insapp"; } | crontab -
 
-crontab -l | { cat; echo "45 3 * * * $(dirname $(readlink -f $0))/insapp-backup/dropbox-uploader/dropbox_uploader.sh -f $(dirname $(readlink -f $0))/insapp-backup/dropbox_uploader.conf upload $(dirname $(readlink -f $0))/insapp-db/backups/`date +%Y-%m-%d`/ /`date +%Y-%m-%d"`/db >> /var/log/dropbox_uploader.log 2>&1"; } | crontab -
+crontab -l | { cat; echo "45 3 * * * $(dirname $(readlink -f $0))/dropbox-uploader/dropbox_uploader.sh -f $(dirname $(readlink -f $0))/dropbox-uploader.conf upload $(dirname $(readlink -f $0))/insapp-db/backups/`date +%Y-%m-%d`/ /`date +%Y-%m-%d"`/db >> /var/log/dropbox-uploader.log 2>&1"; } | crontab -
 
-crontab -l | { cat; echo "0 4 * * * $(dirname $(readlink -f $0))/insapp-backup/dropbox-uploader/dropbox_uploader.sh -f $(dirname $(readlink -f $0))/insapp-backup/dropbox_uploader.conf upload $(dirname $(readlink -f $0))/insapp-cdn/ /`date +%Y-%m-%d"`/cdn >> /var/log/dropbox_uploader.log 2>&1"; } | crontab -
+crontab -l | { cat; echo "0 4 * * * $(dirname $(readlink -f $0))/dropbox-uploader/dropbox_uploader.sh -f $(dirname $(readlink -f $0))/dropbox-uploader.conf upload $(dirname $(readlink -f $0))/insapp-cdn/ /`date +%Y-%m-%d"`/cdn >> /var/log/dropbox-uploader.log 2>&1"; } | crontab -
--- install.sh
+++ install.sh
@@ -3,10 +3,16 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 
-# Prepare insapp-server configuration
+# Copying configuration files
 
 cp docker-compose.yml.dist docker-compose.yml
+cp dropbox-uploader.conf.dist dropbox-uploader.conf
 cp traefik.toml.dist traefik.toml
+cp insapp-web/app/app.config.js.dist insapp-web/app/app.config.js
+cp insapp-go/Dockerfile.dist insapp-go/Dockerfile
+cp insapp-go/src/config.json.dist insapp-go/src/config.json
+
+# Asking for parameters
 
 echo "What should be the domain name that points to this server (insapp.fr):"
 read domain
@@ -29,41 +35,25 @@ read mongo_password
 echo "What is your Dropbox OAuth 2 token (linked to a previously created Dropbox application):"
 read dropbox_token
 
+# Replacing parameters in configuration files
+
 sed -i "s/REPLACE_WITH_THE_HOST_DOMAIN/$domain/g" docker-compose.yml
-sed -i "s/REPLACE_WITH_THE_HOST_DOMAIN/$domain/g" traefik.toml
-
-sed -i "s/REPLACE_WITH_YOUR_GOOGLE_EMAIL/$email/g" traefik.toml
-
 sed -i "s/REPLACE_WITH_THE_API_PORT/$port/g" docker-compose.yml
-
 sed -i "s/REPLACE_WITH_THE_MONGO_PASSWORD/$mongo_password/g" docker-compose.yml
 
-# Prepare insapp-go configuration
+sed -i "s/REPLACE_WITH_YOUR_DROPBOX_TOKEN/$dropbox_token/g" dropbox-uploader.conf
 
-cp insapp-go/Dockerfile.dist insapp-go/Dockerfile
-cp insapp-go/src/config.json.dist insapp-go/src/config.json
-
-sed -i "s/REPLACE_WITH_YOUR_GOOGLE_EMAIL/$email/g" insapp-go/src/config.json
-
-sed -i "s/REPLACE_WITH_YOUR_GOOGLE_PASSWORD/$email_password/g" insapp-go/src/config.json
-
-sed -i "s/REPLACE_WITH_THE_MONGO_PASSWORD/$mongo_password/g" insapp-go/src/config.json
-
-sed -i "s/REPLACE_WITH_THE_ENVIRONMENT_TYPE/$environment/g" insapp-go/src/config.json
-
-sed -i "s/REPLACE_WITH_THE_API_PORT/$port/g" insapp-go/Dockerfile
-sed -i "s/REPLACE_WITH_THE_API_PORT/$port/g" insapp-go/src/config.json
-
-# Prepare insapp-web configuration
-
-cp insapp-web/app/app.config.js.dist insapp-web/app/app.config.js
-
-sed -i "s/REPLACE_WITH_THE_HOST_DOMAIN/$domain/g" insapp-web/app/app.config.js
-
-# Prepare insapp-backup configuration
-
-cp insapp-backup/dropbox_uploader.conf.dist insapp-backup/dropbox_uploader.conf
+sed -i "s/REPLACE_WITH_THE_HOST_DOMAIN/$domain/g" traefik.toml
+sed -i "s/REPLACE_WITH_YOUR_GOOGLE_EMAIL/$email/g" traefik.toml
 
 sed -i "s/REPLACE_WITH_THE_MONGO_PASSWORD/$mongo_password/g" enable_backups.sh
 
-sed -i "s/REPLACE_WITH_YOUR_DROPBOX_TOKEN/$dropbox_token/g" insapp-backup/dropbox_uploader.conf
+sed -i "s/REPLACE_WITH_THE_API_PORT/$port/g" insapp-go/Dockerfile
+
+sed -i "s/REPLACE_WITH_YOUR_GOOGLE_EMAIL/$email/g" insapp-go/src/config.json
+sed -i "s/REPLACE_WITH_YOUR_GOOGLE_PASSWORD/$email_password/g" insapp-go/src/config.json
+sed -i "s/REPLACE_WITH_THE_MONGO_PASSWORD/$mongo_password/g" insapp-go/src/config.json
+sed -i "s/REPLACE_WITH_THE_ENVIRONMENT_TYPE/$environment/g" insapp-go/src/config.json
+sed -i "s/REPLACE_WITH_THE_API_PORT/$port/g" insapp-go/src/config.json
+
+sed -i "s/REPLACE_WITH_THE_HOST_DOMAIN/$domain/g" insapp-web/app/app.config.js
